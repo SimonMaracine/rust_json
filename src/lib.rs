e8b4400626a7ea0c358068eb5a32cc93687b99c7
SimonMaracine <simonmaracine@gmail.com>
2020-12-31 13:29:53 +0200

    Work on the tokenizer; still in progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,161 @@
 mod data_structure;
 
+use std::fs::read_to_string;
+use std::error::Error;
 use data_structure::JSONObject;
 
-pub fn load(file: String) -> JSONObject {
-    JSONObject::new()  // Temporary
+pub fn load(file: String) -> Result<JSONObject, Box<dyn Error>> {
+    let contents = read_to_string(file)?;
+    let tokens = tokenize(&contents);
+    println!("{:#?}", tokens);
+
+
+
+    Ok(JSONObject::new())
 }
 
 pub fn dump(json: JSONObject) -> String {
     String::new()
+}
+
+
+
+fn tokenize(contents: &String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut state = State::InNothing;
+
+    let mut string = String::new();
+    let mut number = String::new();
+    let mut keyword = String::new();
+
+    let mut capture_string_token = false;
+    let mut capture_keyword_token = false;
+
+    // let mut last_character: char = '\0';
+
+    for character in contents.chars() {
+        print!("{} ", character);
+
+        match character {
+            '{' => {
+                if let State::InNumber = state {
+                    tokens.push(Token::Number(number.clone()));
+                    number.clear();
+                    state = State::InNothing;  // Not really in nothing, but not in number
+                }
+
+                if !capture_string_token {
+                    tokens.push(Token::LeftBrace);
+                }
+            }
+            '[' => {
+                if let State::InNumber = state {
+                    tokens.push(Token::Number(number.clone()));
+                    number.clear();
+                    state = State::InNothing;  // Not really in nothing, but not in number
+                }
+
+                if !capture_string_token {
+                    tokens.push(Token::LeftBracket);
+                }
+            }
+            '}' => {
+                if let State::InNumber = state {
+                    tokens.push(Token::Number(number.clone()));
+                    number.clear();
+                    state = State::InNothing;  // Not really in nothing, but not in number
+                }
+
+                if !capture_string_token {
+                    tokens.push(Token::RightBrace);
+                }
+            }
+            ']' => {
+                if let State::InNumber = state {
+                    tokens.push(Token::Number(number.clone()));
+                    number.clear();
+                    state = State::InNothing;  // Not really in nothing, but not in number
+                }
+
+                if !capture_string_token {
+                    tokens.push(Token::RightBracket);
+                }
+            }
+            ':' => {
+                if !capture_string_token {
+                    tokens.push(Token::Colon);
+                }
+            }
+            ',' => {
+                if let State::InNumber = state {
+                    tokens.push(Token::Number(number.clone()));
+                    number.clear();
+                    state = State::InNothing;  // Not really in nothing, but not in number
+                }
+
+                if !capture_string_token {
+                    tokens.push(Token::Comma);
+                }
+            }
+            '"' => { // TODO this can be in string, if there is a \ before
+                if let State::InString = state  {
+                    tokens.push(Token::String(string.clone()));
+                    string.clear();
+                    state = State::InNothing;  // Not really in nothing, but not in string
+                    capture_string_token = false;
+                } else {
+                    state = State::InString;
+                    capture_string_token = true;
+                }
+            }
+            '0' | '1' | '2' | '3' |
+            '4' | '5' | '6' | '7' |
+            '8' | '9' => {
+                if let State::InNumber = state {
+                    number.push(character);
+                } else {
+                    state = State::InNumber;
+                    number.push(character);
+                }
+            }
+            _ => {  // spaces, letters and other characters
+                if capture_string_token {
+                    string.push(character);
+                } else if capture_keyword_token {
+                    keyword.push(character);
+                }
+
+
+            }
+        }
+
+        // last_character = character;
+    }
+
+    tokens
+}
+
+#[derive(Debug)]
+enum Token {
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Colon,
+    Comma,
+    String(String),
+    Number(String),  // Integer or float
+    Keyword(String)  // Boolean or null
+}
+
+enum State {
+    InNothing,
+    InObject,
+    InArray,
+    InString,
+    InNumber,
+    InBool,
+    InNull
 }
 
 #[cfg(test)]
@@ -16,8 +164,9 @@ mod tests {
 
     #[test]
     fn main() {
-        let object = load(String::from("samples/sample1.json"));
-        
+        load(String::from("samples/sample1.json"));
+        println!("####################################");
+        load(String::from("samples/empty.json"));
     }
 
     #[test]
@@ -30,3 +179,29 @@ mod tests {
         assert_eq!(*object.get_int(String::from("Simon")).expect("Is none"), 18);
     }
 }
+
+// match character {
+//     '{' => {
+//         state_stack.push(State::InObject);
+//     },
+
+//     '[' => {
+//         state_stack.push(State::InArray);
+//     },
+//     '"' => {
+//         let state = match state_stack.last() {
+//             Some(s) => s,
+//             None => &State::InNothing
+//         };
+
+//         if let State::InString = *state  {
+//             state_stack.push(State::InString);
+//         } else {
+//             state_stack.pop();
+//         }
+//     },
+//     ']' | '}' => {
+//         state_stack.pop();
+//     },
+//     _ => ()
+// }
